5dc42f46204ab138c411099f477edd64b4b96458
Jvmolu <anmol.singhal50@gmail.com>
2024-12-18 23:37:58 +0530

    dev

--- src/repositories/table-entity-mapper/table-entity-mismatch-mappings/user-profile-schema-mapping.ts
+++ src/repositories/table-entity-mapper/table-entity-mismatch-mappings/user-profile-schema-mapping.ts
@@ -5,6 +5,7 @@ export default {
       { entityField: 'currentAddress', dbField: 'current_address' },
       { entityField: 'currentSalaryAmount', dbField: 'current_salary_amount' },
       { entityField: 'currentSalaryCurrency', dbField: 'current_salary_currency' },
+      { entityField: 'currentYearlySalary', dbField: 'current_yearly_salary' },
       { entityField: 'resumeLink', dbField: 'resume_link' },
       { entityField: 'activelySearching', dbField: 'actively_searching' },
       { entityField: 'workLocationPreference', dbField: 'work_location_preference' },
