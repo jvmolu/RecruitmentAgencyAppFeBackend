f301b2035c5b4e2b89008e528380207486a62e94
Jvmolu <anmol.singhal50@gmail.com>
2024-12-19 01:58:31 +0530

    dev

--- src/sql/createTables/applications.sql
+++ src/sql/createTables/applications.sql
@@ -18,7 +18,9 @@ CREATE TABLE applications (
     cover_letter TEXT NOT NULL,
     status application_status NOT NULL DEFAULT 'ACTIVE',
     stage application_stages NOT NULL DEFAULT 'APPLIED',
-    invite_id UUID DEFAULT NULL REFERENCES invites(id)
+    invite_id UUID DEFAULT NULL REFERENCES invites(id),
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
 -- Add Constraint -> Combination of candidate_id and job_id should be unique
@@ -29,3 +31,4 @@ CREATE INDEX idx_applications_candidate_id ON applications(candidate_id);
 CREATE INDEX idx_applications_job_id ON applications(job_id);
 CREATE INDEX idx_applications_status ON applications(status);
 CREATE INDEX idx_applications_stage ON applications(stage);
+CREATE INDEX idx_applications_created_at ON applications(created_at);
