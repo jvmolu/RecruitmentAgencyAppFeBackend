302064ce2f9b6a5f56829afdd2664ad7fdfc34bc
jvmolu <anmol.singhal50@gmail.com>
2024-12-29 07:46:10 +0000

    minor change

--- src/templates/email-invite.ts
+++ src/templates/email-invite.ts
@@ -4,10 +4,10 @@ export const emailInviteTemplate = (jobId: string, candidateName: string): strin
             <body>
                 <h1>Hi ${candidateName},</h1>
                 <p>You have been invited to apply for a job. Please click the link below to apply:</p> <br>
-                <a href="http://localhost:3000/jobs/${jobId}">Apply for Job</a> <br>
+                <a href="http://134.209.226.138:5173/jobs/${jobId}">Apply for Job</a> <br>
                 If the link does not work, please copy and paste the following URL in your browser:
                 <br> 
-                http://localhost:3000/jobs/${jobId}
+                http://134.209.226.138:5173/jobs/${jobId}
             </body>
         </html>
     `;
